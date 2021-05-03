d4f992f94f95c14843d950a4cb0373a9eae9624f
Agam Jain <jainshibbu2001@gmail.com>
2021-05-03 20:13:19 +0530

    Added some important comments

--- DOM_JIRA/activity/ClassWork/script.js
+++ DOM_JIRA/activity/ClassWork/script.js
@@ -1,3 +1,4 @@
+// Taking elements of html page by querySelector
 let filterOptions = document.querySelectorAll(".filter-colors__container");
 let modalFilters = document.querySelectorAll(".modal_filter");
 let mainContainer = document.querySelector(".main-container");
@@ -10,20 +11,17 @@ let flag = false;
 let deleteState = false;
 let cColor = colors[colors.length - 1];
 
+// Function for setting background color on click
 for (let i = 0; i < filterOptions.length; i++) {
     filterOptions[i].addEventListener("click", function () {
         let arr = filterOptions[i].children;
-        // present classes
-        // console.log(arr[1]);
         let chclassesArr = arr[0].classList;
-
-        // console.log(classesArr[1]);
         mainContainer.style.backgroundColor = chclassesArr[0];
     });
 }
 
 
-
+// function for opening modal on click +
 addBtn.addEventListener("click", function () {
     if (flag == false) {
         modalContainer.style.display = "flex";
@@ -32,24 +30,24 @@ addBtn.addEventListener("click", function () {
     }
     flag = !flag
 })
+
+// function for clicking on modal filters to sleect current colors
 for (let i = 0; i < modalFilters.length; i++) {
     modalFilters[i].addEventListener("click", function () {
+        // Function to remove and add vorder to the particular color in modal
         modalFilters.forEach(function (modalFilter) {
-            // classList remove-> 
             modalFilter.classList.remove("border");
         })
         modalFilters[i].classList.add("border");
         cColor = modalFilters[i].classList[1];
     })
 }
+
+// Functionality of creating ticket on clicking enter
 descBox.addEventListener("keydown", function (e) {
 
     if (e.key == "Enter") {
         let task = descBox.value;
-        // console.log("task is ", task, "cColor ", cColor);
-        // tiket create 
-        // ticket create 
-        //  clean up 
         createTicket(task, cColor);
         cColor = colors[colors.length - 1];
         modalContainer.style.display = "none";
@@ -58,6 +56,8 @@ descBox.addEventListener("keydown", function (e) {
     }
 })
 
+
+// Function which creates ticket and attach its colr and delete functionality to it
 function createTicket(task, cColor){
     let ticketContainer = document.createElement("div");
     ticketContainer.setAttribute("class", "ticket-container");
@@ -71,6 +71,7 @@ function createTicket(task, cColor){
     handleticketDeleteContainer(ticketContainer);
 }
 
+// Function to change color of ticket
 function handleticketColorContainer(ticketContainer){
     // All Nodes also has document functions so
     let colorStripElement = ticketContainer.querySelector(".ticket-color");
@@ -85,7 +86,7 @@ function handleticketColorContainer(ticketContainer){
     })
 }
 
-
+// Remove button functionality
 removeBtn.addEventListener("click", function () {
     if(deleteState == false){
         removeBtn.style.backgroundColor = "rgb(100, 71, 26)";
@@ -95,6 +96,7 @@ removeBtn.addEventListener("click", function () {
     deleteState = !deleteState;
 })
 
+// Particular ticket delete functionality
 function handleticketDeleteContainer(ticketContainer){
     ticketContainer.addEventListener("click", function(){
         if(deleteState == true){
