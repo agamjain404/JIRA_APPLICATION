71c06ee217b022e9c5c9a4fe2beb1d5a3858ad6c
Agam Jain <jainshibbu2001@gmail.com>
2021-05-03 19:49:41 +0530

    Added Delete functionality

--- DOM_JIRA/activity/ClassWork/script.js
+++ DOM_JIRA/activity/ClassWork/script.js
@@ -3,9 +3,11 @@ let modalFilters = document.querySelectorAll(".modal_filter");
 let mainContainer = document.querySelector(".main-container");
 let modalContainer = document.querySelector(".modal_container");
 let addBtn = document.querySelector(".add");
+let removeBtn = document.querySelector(".remove");
 let descBox = document.querySelector(".desc-box");
 let colors = ["lightpink", "lightblue", "lightgreen", "black"];
 let flag = false;
+let deleteState = false;
 let cColor = colors[colors.length - 1];
 
 for (let i = 0; i < filterOptions.length; i++) {
@@ -65,10 +67,11 @@ function createTicket(task, cColor){
                 <p class="ticket-desc">${task}</p>
             </div>`;
     mainContainer.appendChild(ticketContainer);
-    handleContainer(ticketContainer);
+    handleticketColorContainer(ticketContainer);
+    handleticketDeleteContainer(ticketContainer);
 }
 
-function handleContainer(ticketContainer){
+function handleticketColorContainer(ticketContainer){
     // All Nodes also has document functions so
     let colorStripElement = ticketContainer.querySelector(".ticket-color");
     colorStripElement.addEventListener("click", function(){
@@ -79,5 +82,23 @@ function handleContainer(ticketContainer){
         let newColor = colors[newidx];
         colorStripElement.classList.remove(initColor);
         colorStripElement.classList.add(newColor);
+    })
+}
+
+
+removeBtn.addEventListener("click", function () {
+    if(deleteState == false){
+        removeBtn.style.backgroundColor = "rgb(100, 71, 26)";
+    }else{
+        removeBtn.style.backgroundColor = "rgb(146, 102, 35)";
+    }
+    deleteState = !deleteState;
+})
+
+function handleticketDeleteContainer(ticketContainer){
+    ticketContainer.addEventListener("click", function(){
+        if(deleteState == true){
+            ticketContainer.remove();
+        }
     })
 }
